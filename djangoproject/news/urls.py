314cd5392be612be8842cffe39c24da1c4cdef60
ago1256 <ago1256@yandex.ru>
2025-04-25 18:50:27 +0300

    7-9

--- djangoproject/news/urls.py
+++ djangoproject/news/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.news_home, name ='news_home')
+   path('', views.news_home, name ='news_home'),
+   path('create', views.create, name ='create'),
 ]
